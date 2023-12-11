2aae55e1a7843d018c788fdbefb37c0487289ca3
ZuLu29 <markneilconde850@gmail.com>
2023-12-11 21:33:02 +0800

    modified store entry and class user info

--- ITE1_FINAL_CONDE/Program.cs
+++ ITE1_FINAL_CONDE/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,13 +18,13 @@ namespace ITE1_FINAL_CONDE
         public int AreaCode { get; set; }
         public long PhoneNumber { get; set; }
 
-        public UserInfo(string studentNumber, string surname, string firstname, string occumation, char gender, int countryCode, int areaCode, long phoneNumber)
+        public UserInfo(string studentNumber, string surname, string firstname, string occupation, char gender, int countryCode, int areaCode, long phoneNumber)
         {
             StudentNumber = studentNumber;
             Surname = surname;
             FirstName = firstname;
-            Occupation = occumation;
-            Gender = gender;
+            Occupation = occupation;
+            Gender = char.ToUpper(gender);
             CountryCode = countryCode;
             AreaCode = areaCode;
             PhoneNumber = phoneNumber;
@@ -37,28 +38,40 @@ namespace ITE1_FINAL_CONDE
         {
             do
             {
-                UserInfo usersInfo = new UserInfo();
-                Console.Write("Enter Student Number: ");
-                usersInfo.StudentNumber = Console.ReadLine();
-                Console.Write("Enter Surname: ");
-                usersInfo.Surname = Console.ReadLine();
-                Console.Write("Enter First Name: ");
-                usersInfo.FirstName = Console.ReadLine();
-                Console.Write("Enter Occupation: ");
-                usersInfo.Occupation = Console.ReadLine();
-                Console.Write("Enter your gender (M) for male, (F) for female: ");
-                usersInfo.Gender = char.Parse(Console.ReadLine());
-                Console.Write("Enter Country Code: ");
-                usersInfo.CountryCode = int.Parse(Console.ReadLine());
-                Console.Write("Enter Area Code: ");
-                usersInfo.AreaCode = int.Parse(Console.ReadLine());
-                Console.Write("Enter Number: ");
-                usersInfo.PhoneNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Provide the student details below.");
 
-                users.Add(usersInfo);
+                Console.Write("Enter student number: ");
+                string studentNumber = Console.ReadLine();
 
-                Console.Write("Do you want to enter another entry (Y/N)? ");
-            } while (Console.ReadLine().ToUpper() == "Y");
+                Console.Write("Enter surname: ");
+                string surname = Console.ReadLine();
+
+                Console.Write("Enter firstname: ");
+                string firstname = Console.ReadLine();
+
+                Console.Write("Enter occupation: ");
+                string occupation = Console.ReadLine();
+
+                Console.Write("Enter gender (M for male F for female): ");
+                char gender = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                Console.Write("Enter country code: ");
+                int countryCode = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter area code: ");
+                int areaCode = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter number: ");
+                long phoneNumber = long.Parse(Console.ReadLine());
+
+                UserInfo entry = new UserInfo(studentNumber, surname, firstname, occupation, gender, countryCode, areaCode, phoneNumber);
+                users.Add(entry);
+
+                Console.Write("Do you want to add another entry [Y/N]? ");
+            } while (char.ToUpper(Console.ReadKey().KeyChar) == 'Y');
+
+            Console.WriteLine();
         }
         public void EditEntry()
         {
@@ -75,7 +88,7 @@ namespace ITE1_FINAL_CONDE
             {
                 Console.WriteLine($"Here is the existing information about {userNumberEdit}:");
                 string pronoun = (userNumberEdit.gender == "M") ? "His" : "her";
-            }
+            } 
         }
 
 
@@ -103,13 +116,7 @@ namespace ITE1_FINAL_CONDE
                             phonebook.StoreEntry();
                             break;
                         case 2:
-                            phonebook.StoreEntry();
-                            break;
-                        case 3:
-                            phonebook.StoreEntry();
-                            break;
-                        case 4:
-                            phonebook.StoreEntry();
+                            phonebook.EditEntry();
                             break;
                     }
                 }
